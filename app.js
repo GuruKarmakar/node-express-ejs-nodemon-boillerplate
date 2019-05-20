9d9144bad2c76ed5f9d1b4891af542aa737a368a
Guru Karmakar <djguruwap@gmail.com>
2019-05-20 10:56:18 +0530

    fix port

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+//a very simple boilerplate by Guru karmakar
+//for any kind of help  : #email me (djguruwap@gmail.com)
+
 const express =  require('express')
 const app = express()
 const path =(require('path'))
 //port 
-const port = 3030
+const port = 4000
 
 //set up default engine as ejs
 app.set('view engine','ejs')
