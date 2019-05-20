f9ecd86f9fcd2fb69d7d80edd3ff71c72e337e03
Guru Karmakar <djguruwap@gmail.com>
2019-05-20 10:11:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express =  require('express')
+const app = express()
+const path =(require('path'))
+//port 
+const port = 3000
+
+//set up default engine as ejs
+app.set('view engine','ejs')
+app.set('views','views')
+
+//sarve html statically
+app.use(express.static(path.join(__dirname,'public')))
+
+//router
+const indexRouter = require('./routes/index')
+
+app.use(indexRouter)
+
+app.listen(port , ()=>{
+    console.log(`Server running on ${port}`)
+})
